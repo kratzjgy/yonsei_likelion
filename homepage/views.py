f3cb48a7dde84e505d92d191778ff4509c3f8090
Gu Young <484455@likelion.org>
2018-12-20 12:21:56 +0000

    템플릿 적용

--- homepage/views.py
+++ homepage/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.utils import timezone
+from .models import Homepage
 # Create your views here.
 
 def index(request):
