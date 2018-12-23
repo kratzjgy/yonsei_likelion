8091af30b46aaeb36566b50712dffc6d720850c4
Hayeon <hayeon226@likelion.org>
2018-12-23 15:28:09 +0000

    hihii

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,10 +2,8 @@ from django.conf.urls import url
 from . import views
   
 urlpatterns = [
-<<<<<<<HEAD
-    url(r'^$',views.index,name = 'index.html'),
-=======
+
     url(r'^$', views.index, name='index'),
     url(r'^about/',views.about, name='about'),
->>>>>>> d56dfe54ac1b9cf803986ad5fec8dc620437e78c
+
 ]
