a6c9ca192443a20297e386501ef3a560d61d65bf
Hayeon <hayeon226@likelion.org>
2018-12-25 08:14:09 +0000

    방명록 수정중

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url 
+from django.contrib import admin
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
   
 urlpatterns = [
-
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-    url(r'^about/',views.about, name='about'),
-
+    url(r'^about/$',views.about, name='about'),
+    url(r'^post/', views.post, name='post'),
+    url(r'^(?P<pk>\d+)/$', views.post_detail),
+    url(r'^(?P<pk>\d+)/comments/new/$', views.comment_new),
+    url(r'^(?P<post_pk>\d+)/comments/(?P<pk>\d+)/edit/$', views.comment_edit)
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.utils import timezone
 from .models import Homepage
+from .models import Post
+from .models import Comment
+#from .models import .
+from .forms import CommentForm
 # Create your views here.
 
 def index(request):
@@ -14,4 +19,46 @@ def index(request):
 
 def about(request):
     return render(request,'homepage/about.html',{}) 
+
+def post(request):
+    post_list = Post.objects.all()
+    return render(request, 'homepage/post.html', {
+        'post_list': post_list,
+    })
     
+def post_detail(request, pk):
+    post = Post.objects.get(pk = pk)
+    return render(request, 'homepage/post_detail.html', {
+        'post': post,
+    })
+    
+def comment_new(request, pk):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = Post.objects.get(pk=pk)
+            comment.save()
+            return redirect('homepage.views.post_detail', pk)
+    else:
+        form = CommentForm()
+    return render(request, 'homepage/post_form.html', {
+        'form' : form,
+    })
+        
+def comment_edit(request, post_pk, pk):
+    comment = Comment.objects.get(pk=pk)
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = Post.objects.get(pk=post_pk)
+            comment.save()
+            return redirect('homepage.views.post_detail', post_pk)
+    else:
+        form = CommentForm(instance = comment)
+    return render(request, 'homepage/post_form.html', {
+        'form' : form,
+    })
+            
